b0aba615b3950429b3fa070354c771c439618615
Antonio Mejia <avmejia@tec.mx>
2021-04-18 18:59:26 -0500

    inicial

---
+++ 0-Makefile/claseA.h
@@ -0,0 +1,11 @@
+#ifndef CLASEA_H_
+#define CLASEA_H_
+
+
+class ClaseA
+{
+    public:
+    ClaseA();
+    void imprimir();
+};
+#endif
---
+++ 0-Makefile/claseB.h
@@ -0,0 +1,10 @@
+#ifndef CLASEB_H_
+#define CLASEB_H_
+
+class ClaseB
+{
+    public:
+    ClaseB();
+    void imprimir();
+};
+#endif
